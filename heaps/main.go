06d08b27976059b4ef433ceae7eb12e285b26a15
nathanmbicho <nathanmbicho@gmail.com>
2021-07-03 07:29:03 +0300

    golang heaps test

---
+++ heaps/main.go
@@ -0,0 +1,118 @@
+/*
+Heaps :- a data structure that can be expressed as a complete tree meaning (complete tree) all the levels
+		of the tree are full except for the lowest level
+Heaps good for implementing priority queues
+Priority Queues - highest priority is served first
+Normal Queues - first in first out(FIFO)
+
+Max heap :- highest key is the root
+Min heap :- lowest key is the root
+*/
+package main
+
+import "fmt"
+
+// MaxHeap struct has a slice  that holds the array
+	type MaxHeap struct{
+		array []int
+	}
+
+	// Insert adds  an element  to the heap
+	func (h *MaxHeap) Insert(key int){
+		h.array = append(h.array, key) // append the new key to array
+		h.maxHeapifyUp(len(h.array)-1)
+	}
+
+	// Extract returns the largest key, and removes it from the heap
+	func (h *MaxHeap) Extract() int{
+		extracted := h.array[0]
+		l := len(h.array) - 1
+
+		// when the array is empty
+		if len(h.array) == 0{
+			fmt.Println("cannot extract because array length is 0")
+			return -1
+		}
+
+		// take out the last index and put it in the root
+		h.array[0] = h.array[l]
+		h.array = h.array[:l]
+
+		h.maxHeapifyDown(0)
+
+		return extracted
+	}
+
+	// maxHeapifyUp will heapify from bottom to top
+	func (h *MaxHeap) maxHeapifyUp(index int){
+		// when the parent index is smaller than the current index
+		for h.array[parent(index)] < h.array[index] {
+			h.swap(parent(index), index)
+			index = parent(index)
+		}
+	}
+
+	// maxHeapifyDown will heapify from top to bottom
+	func (h *MaxHeap) maxHeapifyDown(index int){
+
+		lastIndex := len(h.array) - 1
+		l, r := left(index), right(index)
+		childToCompare := 0
+
+		// loop while index has at least one child
+		for l <= lastIndex {
+			if l == lastIndex { // when left child is the only child
+				childToCompare = l
+			}else if h.array[l] > h.array[r] { // when left child is langer
+				childToCompare = l
+			}else { // when right child is larger
+				childToCompare = r
+			}
+
+			// compare array value of current index to larger child and swap if smaller
+			if h.array[index] < h.array[childToCompare] {
+				h.swap(index, childToCompare)
+				index = childToCompare
+				l, r = left(index), right(index)
+			}else{
+				return
+			}
+
+		}
+	}
+
+	// get the parent index function
+	func parent(i int) int{
+		return (i-1) / 2
+	}
+
+	// get the left child index function -left side always a negative number
+	func left(i int) int{
+		return 2*i + 1
+	}
+
+	// get the right child index function -right side always a positive number
+	func right(i int) int{
+		return 2*i + 2
+	}
+
+	//swap keys in the array
+	func (h *MaxHeap) swap(i1, i2 int){
+		h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+	}
+
+func main(){
+	m := &MaxHeap{}
+	fmt.Println(m)
+	buildHeap := []int{10, 20, 30, 5, 7, 9, 11, 13, 15, 17};
+	for _, v := range buildHeap{
+		m.Insert(v)
+		fmt.Println(m)
+	}
+
+	// extract
+	for i := 0; i < 5; i++{
+		m.Extract()
+		fmt.Println(m)
+	}
+}
